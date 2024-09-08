793f8b6ef5e5c5140e1d68459b62722e70a7fa3a
Dainan Lucas <dainan.lucas@gmail.com>
2024-09-08 17:32:00 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,15 @@
+let dados = [
+    
+    "Rayssa Leal" ,
+     "Rebeca Andrade" ,
+      " Beatriz Souza"
+
+];
+
+let rayssa = {
+
+    titulo : "Raysa Leal" ,
+    descricao : "Tive um imprevisto e não consegui chegar a tempo para a reunião de hoje.Você acha que seria possível reagendar a quinta-feira às 14h?" ,
+    link : "https://eueduco.renovationefficient.com",
+    
+}
